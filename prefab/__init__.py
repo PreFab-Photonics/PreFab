83ca97097c9c8b5076f97e31afac5ceabccbf31a
Dusan Gostimirovic <dusan.gostimirovic@gmail.com>
2024-03-09 14:16:51 -0500

    Sort and cleanup

--- prefab/__init__.py
+++ prefab/__init__.py
@@ -6,16 +6,19 @@ Usage:
     import prefab as pf
 """
 
-# Prediction and Correction
-from prefab.predictor import predict
-from prefab.predictor import correct
-
-# I/O Utilities
+# I/O utilities
 # Function to load/save device images and gds files
 from prefab.io import (
-    load_device_img,  # Load device from an image file
-    load_device_gds,  # Load device from a GDSII file
     device_to_cell,  # Convert a device layout to a gdstk cell
+    load_device_gds,  # Load device from a GDSII file
+    load_device_img,  # Load device from an image file
+)
+
+# Prediction and Correction
+# Functions to predict and correct device layouts
+from prefab.predictor import (
+    correct,  # Correct a device layout
+    predict,  # Predict a device layout
 )
 
 # Import image processing utilities
@@ -23,25 +26,26 @@ from prefab.io import (
 from prefab.processor import (
     binarize,  # Soft binarization of grayscale images
     binarize_hard,  # Hard binarization of grayscale images
-    ternarize,  # Ternarization of grayscale images
-    remove_padding,  # Trims excess padding from device images
-    zero_boundary,  # Applies zero boundary to device images
-    generate_device_contour,  # Generates contour of device images
     calculate_prediction_uncertainty,  # Computes prediction uncertainty for device images
+    generate_device_contour,  # Generates contour of device images
     mse,  # Computes mean squared error between two images
+    remove_padding,  # Trims excess padding from device images
+    ternarize,  # Ternarization of grayscale images
+    zero_boundary,  # Applies zero boundary to device images
 )
 
 __all__ = (
-    "predict",
-    "load_device_img",
-    "load_device_gds",
     "device_to_cell",
+    "load_device_gds",
+    "load_device_img",
+    "correct",
+    "predict",
     "binarize",
     "binarize_hard",
-    "ternarize",
-    "remove_padding",
-    "zero_boundary",
-    "generate_device_contour",
     "calculate_prediction_uncertainty",
+    "generate_device_contour",
     "mse",
+    "remove_padding",
+    "ternarize",
+    "zero_boundary",
 )
