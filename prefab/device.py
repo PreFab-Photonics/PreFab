a124b1d98b260bed4f73092b46c74f63762ecd5d
Dusan Gostimirovic <dusan.gostimirovic@gmail.com>
2024-03-29 10:45:09 -0400

    Cleanup and documentation

--- prefab/device.py
+++ prefab/device.py
@@ -10,10 +10,47 @@ from matplotlib.lines import Line2D
 from matplotlib.patches import Rectangle
 from pydantic import BaseModel, Field, conint, root_validator, validator
 
-from prefab import geometry
+from . import geometry
 
 
 class BufferSpec(BaseModel):
+    """Defines the specifications for a buffer zone around a device.
+
+    This class is used to specify the mode and thickness of a buffer zone that is added
+    around the device geometry. The buffer zone can be used for various purposes such as
+    providing extra space for device fabrication processes or for ensuring that the
+    device is isolated from surrounding structures.
+
+    Parameters
+    ----------
+    mode : dict[str, str]
+        A dictionary that defines the buffer mode for each side of the device
+        ('top', 'bottom', 'left', 'right'), where 'constant' is used for isolated
+        structures and 'edge' is utilized for preserving the edge, such as for waveguide
+        connections.
+    thickness : conint(gt=0)
+        The thickness of the buffer zone around the device. Must be greater than 0.
+
+    Raises
+    ------
+    ValueError
+        If any of the modes specified in the 'mode' dictionary are not one of the
+        allowed values ('constant', 'edge'). Or if the thickness is not greater than 0.
+
+    Example
+    -------
+    >>> import prefab as pf
+    >>> buffer_spec = pf.BufferSpec(
+    ...     mode={
+    ...         "top": "constant",
+    ...         "bottom": "edge",
+    ...         "left": "constant",
+    ...         "right": "edge"
+    ...     },
+    ...     thickness=150
+    ... )
+    """
+
     mode: dict[str, str] = Field(
         default_factory=lambda: {
             "top": "constant",
@@ -33,14 +70,49 @@ class BufferSpec(BaseModel):
 
 
 class Device(BaseModel):
-    device_array: np.ndarray
+    device_array: np.ndarray = Field(...)
     buffer_spec: BufferSpec = Field(default_factory=BufferSpec)
 
     class Config:
         arbitrary_types_allowed = True
 
-    def __init__(self, **data):
-        super().__init__(**data)
+    def __init__(
+        self, device_array: np.ndarray, buffer_spec: Optional[BufferSpec] = None
+    ):
+        """
+        Represents the planar geometry of a photonic device design that will have its
+        nanofabrication outcome predicted and/or corrected.
+
+        This class is designed to encapsulate the geometric representation of a photonic
+        device, facilitating operations such as padding, normalization, binarization,
+        ternarization, trimming, and blurring. These operations are useful for preparing
+        the device design for prediction or correction. Additionally, the class provides
+        methods for exporting the device representation to various formats, including
+        ndarray, image files, and GDSII files, supporting a range of analysis and
+        fabrication workflows.
+
+        Parameters
+        ----------
+        device_array : np.ndarray
+            A 2D array representing the planar geometry of the device. This array
+            undergoes various transformations to predict or correct the nanofabrication
+            process.
+        buffer_spec : BufferSpec, optional
+            Defines the parameters for adding a buffer zone around the device geometry.
+            This buffer zone is needed for providing surrounding context for prediction
+            or correction and for ensuring seamless integration with the surrounding
+            circuitry. By default, a generous padding is applied to accommodate isolated
+            structures.
+
+        Raises
+        ------
+        ValueError
+            If the provided `device_array` is not a numpy ndarray or is not a 2D array,
+            indicating an invalid device geometry.
+        """
+        super().__init__(
+            device_array=device_array, buffer_spec=buffer_spec or BufferSpec()
+        )
         self._initial_processing()
 
     def _initial_processing(self):
@@ -49,22 +121,22 @@ class Device(BaseModel):
 
         self.device_array = np.pad(
             self.device_array,
-            ((buffer_thickness, 0), (0, 0)),
+            pad_width=((buffer_thickness, 0), (0, 0)),
             mode=buffer_mode["top"],
         )
         self.device_array = np.pad(
             self.device_array,
-            ((0, buffer_thickness), (0, 0)),
+            pad_width=((0, buffer_thickness), (0, 0)),
             mode=buffer_mode["bottom"],
         )
         self.device_array = np.pad(
             self.device_array,
-            ((0, 0), (buffer_thickness, 0)),
+            pad_width=((0, 0), (buffer_thickness, 0)),
             mode=buffer_mode["left"],
         )
         self.device_array = np.pad(
             self.device_array,
-            ((0, 0), (0, buffer_thickness)),
+            pad_width=((0, 0), (0, buffer_thickness)),
             mode=buffer_mode["right"],
         )
 
@@ -73,31 +145,53 @@ class Device(BaseModel):
     @root_validator(pre=True)
     def check_device_array(cls, values):
         device_array = values.get("device_array")
-        if device_array is not None:
-            if not isinstance(device_array, np.ndarray):
-                raise ValueError("device_array must be a numpy ndarray.")
-            if device_array.ndim != 2:
-                raise ValueError("device_array must be a 2D array.")
+        if not isinstance(device_array, np.ndarray):
+            raise ValueError("device_array must be a numpy ndarray.")
+        if device_array.ndim != 2:
+            raise ValueError("device_array must be a 2D array.")
         return values
 
     def to_ndarray(self) -> np.ndarray:
+        """Converts the device geometry to a numpy ndarray.
+
+        This method applies the buffer specifications to crop the device array if
+        necessary, based on the buffer mode ('edge' or 'constant'). It then returns the
+        resulting numpy ndarray representing the device geometry.
+
+        Returns
+        -------
+        np.ndarray
+            The numpy ndarray representation of the device geometry, with any applied
+            buffer cropping.
+        """
         device_array = np.copy(self.device_array)
         buffer_thickness = self.buffer_spec.thickness
         buffer_mode = self.buffer_spec.mode
 
-        if buffer_mode["top"] == "edge":
-            device_array[0:buffer_thickness, :] = 0
-        if buffer_mode["bottom"] == "edge":
-            device_array[-buffer_thickness:, :] = 0
-        if buffer_mode["left"] == "edge":
-            device_array[:, 0:buffer_thickness] = 0
-        if buffer_mode["right"] == "edge":
-            device_array[:, -buffer_thickness:] = 0
+        crop_top = buffer_thickness if buffer_mode["top"] == "edge" else 0
+        crop_bottom = buffer_thickness if buffer_mode["bottom"] == "edge" else 0
+        crop_left = buffer_thickness if buffer_mode["left"] == "edge" else 0
+        crop_right = buffer_thickness if buffer_mode["right"] == "edge" else 0
 
-        ndarray = geometry.trim(device_array=device_array)
+        ndarray = device_array[
+            crop_top : device_array.shape[0] - crop_bottom,
+            crop_left : device_array.shape[1] - crop_right,
+        ]
         return ndarray
 
     def to_img(self, img_path: str = "prefab_device.png"):
+        """Exports the device geometry as an image file.
+
+        This method converts the device geometry to a numpy ndarray using `to_ndarray`,
+        scales the values to the range [0, 255] for image representation, and saves the
+        result as an image file.
+
+        Parameters
+        ----------
+        img_path : str, optional
+            The path where the image file will be saved. If not specified, the image is
+            saved as "prefab_device.png" in the current directory.
+        """
         cv2.imwrite(img_path, 255 * self.to_ndarray())
         print(f"Saved Device to '{img_path}'")
 
@@ -108,6 +202,27 @@ class Device(BaseModel):
         gds_layer: tuple[int, int] = (1, 0),
         contour_approx_mode: int = 2,
     ):
+        """Exports the device geometry as a GDSII file.
+
+        This method converts the device geometry into a format suitable for GDSII files.
+        The conversion involves contour approximation to simplify the geometry while
+        preserving essential features.
+
+        Parameters
+        ----------
+        gds_path : str, optional
+            The path where the GDSII file will be saved. If not specified, the file is
+            saved as "prefab_device.gds" in the current directory.
+        cell_name : str, optional
+            The name of the cell within the GDSII file. If not specified, defaults to
+            "prefab_device".
+        gds_layer : tuple[int, int], optional
+            The layer and datatype to use within the GDSII file. Defaults to (1, 0).
+        contour_approx_mode : int, optional
+            The mode of contour approximation used during the conversion. Defaults to 2,
+            which corresponds to `cv2.CHAIN_APPROX_SIMPLE`, a method that compresses
+            horizontal, vertical, and diagonal segments and leaves only their endpoints.
+        """
         gdstk_cell = self._device_to_gdstk(
             cell_name=cell_name,
             gds_layer=gds_layer,
@@ -124,6 +239,29 @@ class Device(BaseModel):
         gds_layer: tuple[int, int] = (1, 0),
         contour_approx_mode: int = 2,
     ):
+        """Converts the device geometry to a GDSTK cell object.
+
+        This method prepares the device geometry for GDSII file export by converting it
+        into a GDSTK cell object. GDSTK is a Python module for creating and manipulating
+        GDSII layout files. The conversion involves contour approximation to simplify
+        the geometry while preserving essential features.
+
+        Parameters
+        ----------
+        cell_name : str, optional
+            The name of the cell to be created. Defaults to "prefab_device".
+        gds_layer : tuple[int, int], optional
+            The layer and datatype to use within the GDSTK cell. Defaults to (1, 0).
+        contour_approx_mode : int, optional
+            The mode of contour approximation used during the conversion. Defaults to 2,
+            which corresponds to `cv2.CHAIN_APPROX_SIMPLE`, a method that compresses
+            horizontal, vertical, and diagonal segments and leaves only their endpoints.
+
+        Returns
+        -------
+        gdstk.Cell
+            The GDSTK cell object representing the device geometry.
+        """
         gdstk_cell = self._device_to_gdstk(
             cell_name=cell_name,
             gds_layer=gds_layer,
@@ -199,6 +337,29 @@ class Device(BaseModel):
     def plot(
         self, show_buffer: bool = True, ax: Optional[Axes] = None, **kwargs
     ) -> Axes:
+        """Visualizes the device along with its buffer zones.
+
+        This method plots the device geometry, allowing for the visualization of the
+        device along with its buffer zones if specified. The plot can be customized with
+        various matplotlib parameters and can be drawn on an existing matplotlib Axes
+        object or create a new one if none is provided.
+
+        Parameters
+        ----------
+        show_buffer : bool, optional
+            If True, the buffer zones around the device will be visualized. This can
+            help in understanding the spatial context of the device within its buffer.
+            By default, it is set to True.
+        ax : Optional[Axes], optional
+            An existing matplotlib Axes object to draw the device geometry on. If None,
+            a new figure and axes will be created. Defaults to None.
+
+        Returns
+        -------
+        Axes
+            The matplotlib Axes object containing the plot. This can be used for further
+            customization or saving the plot after the method returns.
+        """
         ax = self._plot_base(show_buffer=show_buffer, ax=ax, **kwargs)
         _ = ax.imshow(
             self.device_array,
@@ -215,6 +376,33 @@ class Device(BaseModel):
         ax: Optional[Axes] = None,
         **kwargs,
     ):
+        """Visualizes the contour of the device along with optional buffer zones.
+
+        This method plots the contour of the device geometry, emphasizing the edges and
+        boundaries of the device. The contour plot can be customized with various
+        matplotlib parameters, including line width and color. The plot can be drawn on
+        an existing matplotlib Axes object or create a new one if none is provided.
+
+        Parameters
+        ----------
+        linewidth : Optional[int], optional
+            The width of the contour lines. If None, the linewidth is automatically
+            determined based on the size of the device array. Defaults to None.
+        label : Optional[str], optional
+            The label for the contour in the plot legend. Defaults to "Device contour".
+        show_buffer : bool, optional
+            If True, the buffer zones around the device will be visualized. By default,
+            it is set to True.
+        ax : Optional[Axes], optional
+            An existing matplotlib Axes object to draw the device contour on. If None, a
+            new figure and axes will be created. Defaults to None.
+
+        Returns
+        -------
+        Axes
+            The matplotlib Axes object containing the contour plot. This can be used for
+            further customization or saving the plot after the method returns.
+        """
         ax = self._plot_base(show_buffer=show_buffer, ax=ax, **kwargs)
         kwargs.setdefault("cmap", "spring")
         if linewidth is None:
@@ -228,7 +416,6 @@ class Device(BaseModel):
         contour_array = np.zeros_like(self.device_array, dtype=np.uint8)
         cv2.drawContours(contour_array, contours, -1, (255,), linewidth)
         contour_array = np.ma.masked_equal(contour_array, 0)
-
         _ = ax.imshow(
             contour_array,
             extent=[0, self.device_array.shape[1], 0, self.device_array.shape[0]],
@@ -243,6 +430,32 @@ class Device(BaseModel):
     def plot_uncertainty(
         self, show_buffer: bool = True, ax: Optional[Axes] = None, **kwargs
     ):
+        """Visualizes the uncertainty in the edge positions of the predicted device.
+
+        This method plots the predicted device geometry with an overlay indicating the
+        uncertainty associated with the positions of the edges of the device. The
+        uncertainty is represented as a gradient, with areas of higher uncertainty
+        indicating a greater likelihood of variation in edge position from run to run
+        due to inconsistencies in the fabrication process. This visualization can help
+        in identifying areas within the device geometry that may require design
+        adjustments to improve fabrication consistency.
+
+        Parameters
+        ----------
+        show_buffer : bool, optional
+            If True, the buffer zones around the device will also be visualized. By
+            default, it is set to True.
+        ax : Optional[Axes], optional
+            An existing matplotlib Axes object to draw the uncertainty visualization on.
+            If None, a new figure and axes will be created. Defaults to None.
+
+        Returns
+        -------
+        Axes
+            The matplotlib Axes object containing the uncertainty visualization. This
+            can be used for further customization or saving the plot after the method
+            returns.
+        """
         ax = self._plot_base(show_buffer=show_buffer, ax=ax, **kwargs)
 
         uncertainty_array = 1 - 2 * np.abs(0.5 - self.device_array)
@@ -312,28 +525,93 @@ class Device(BaseModel):
         ax.add_patch(right_rect)
 
     def normalize(self) -> "Device":
+        """Normalize the device geometry.
+
+        Returns
+        -------
+        Device
+            A new instance of the Device with the normalized geometry.
+        """
         normalized_device_array = geometry.normalize(device_array=self.device_array)
         return self.model_copy(update={"device_array": normalized_device_array})
 
     def binarize(self, eta: float = 0.5, beta: float = np.inf) -> "Device":
+        """
+        Binarize the device geometry based on a threshold and a scaling factor.
+
+        Parameters
+        ----------
+        eta : float, optional
+            The threshold value for binarization. Defaults to 0.5.
+        beta : float, optional
+            The scaling factor for the binarization process. A higher value makes the
+            transition sharper. Defaults to np.inf, which results in a hard threshold.
+
+        Returns
+        -------
+        Device
+            A new instance of the Device with the binarized geometry.
+        """
         binarized_device_array = geometry.binarize(
             device_array=self.device_array, eta=eta, beta=beta
         )
         return self.model_copy(update={"device_array": binarized_device_array})
 
     def binarize_hard(self, eta: float = 0.5) -> "Device":
+        """
+        Apply a hard threshold to binarize the device geometry.
+
+        Parameters
+        ----------
+        eta : float, optional
+            The threshold value for binarization. Defaults to 0.5.
+
+        Returns
+        -------
+        Device
+            A new instance of the Device with the threshold-binarized geometry.
+        """
         binarized_device_array = geometry.binarize_hard(
             device_array=self.device_array, eta=eta
         )
         return self.model_copy(update={"device_array": binarized_device_array})
 
     def ternarize(self, eta1: float = 1 / 3, eta2: float = 2 / 3) -> "Device":
+        """
+        Ternarize the device geometry based on two thresholds.
+
+        Parameters
+        ----------
+        eta1 : float, optional
+            The first threshold value for ternarization. Defaults to 1/3.
+        eta2 : float, optional
+            The second threshold value for ternarization. Defaults to 2/3.
+
+        Returns
+        -------
+        Device
+            A new instance of the Device with the ternarized geometry.
+        """
         ternarized_device_array = geometry.ternarize(
             device_array=self.device_array, eta1=eta1, eta2=eta2
         )
         return self.model_copy(update={"device_array": ternarized_device_array})
 
     def trim(self) -> "Device":
+        """
+        Trim the device geometry by removing empty space around it.
+
+        Parameters
+        ----------
+        buffer_thickness : int, optional
+            The thickness of the buffer to leave around the empty space. Defaults to 0,
+            which means no buffer is added.
+
+        Returns
+        -------
+        Device
+            A new instance of the Device with the trimmed geometry.
+        """
         trimmed_device_array = geometry.trim(
             device_array=self.device_array,
             buffer_thickness=self.buffer_spec.thickness,
@@ -341,6 +619,20 @@ class Device(BaseModel):
         return self.model_copy(update={"device_array": trimmed_device_array})
 
     def blur(self, sigma: float = 1.0) -> "Device":
+        """
+        Apply Gaussian blur to the device geometry and normalize the result.
+
+        Parameters
+        ----------
+        sigma : float, optional
+            The standard deviation for the Gaussian kernel. This controls the amount of
+            blurring. Defaults to 1.0.
+
+        Returns
+        -------
+        Device
+            A new instance of the Device with the blurred and normalized geometry.
+        """
         blurred_device_array = geometry.blur(
             device_array=self.device_array, sigma=sigma
         )
