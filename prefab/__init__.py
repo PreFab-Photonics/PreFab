ffef924f5ab5f9aa27b5a03a572265eb56998bcd
Dusan Gostimirovic <dusan.gostimirovic@gmail.com>
2024-03-28 21:30:35 -0400

    Cleanup

--- prefab/__init__.py
+++ prefab/__init__.py
@@ -1,20 +1,16 @@
 """
-The prefab module provides functionality for predicting and correcting nanofabrication
-variations in photonics device designs.
+The prefab module predicts and corrects nanofabrication variations in photonic devices.
 
 Usage:
     import prefab as pf
 """
 
-from prefab import geometry, io, predictor, processor, read
+from prefab import geometry, read
 from prefab.device import BufferSpec, Device
 
 __all__ = [
     "Device",
     "BufferSpec",
     "geometry",
-    "io",
-    "predictor",
-    "processor",
     "read",
 ]
