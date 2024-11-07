09d26f7cb5f1db4cf5d746d64a85abf7c7592467
Dusan Gostimirovic <dusan.gostimirovic@gmail.com>
2024-11-07 17:02:30 -0500

    Docstring improvements and nicer welcome message

--- prefab/__main__.py
+++ prefab/__main__.py
@@ -1,4 +1,4 @@
-"""Provides the main entry point for the Prefab CLI."""
+"""Provides the main entry point for the Prefab authentication CLI."""
 
 import argparse
 import os
@@ -10,13 +10,17 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 import toml
 
 
-def store_jwt_securely(jwt, refresh_token):
-    """Store the JWT and refresh token securely in a TOML file."""
-
+def store_jwt(jwt, refresh_token):
+    """Store the JWT and refresh token in a TOML file."""
     prefab_file_path = os.path.expanduser("~/.prefab.toml")
     with open(prefab_file_path, "w", encoding="utf-8") as toml_file:
         toml.dump({"access_token": jwt, "refresh_token": refresh_token}, toml_file)
-    print(f"Token successfully stored in {prefab_file_path}")
+    print(
+        f"Token successfully stored in {prefab_file_path}.\n\n"
+        "🎉 Welcome to PreFab!.\n"
+        "See our examples at https://docs.prefabphotonics.com/examples to start.\n"
+        "Reach out to us at hi@prefabphotonics.com if you have any questions."
+    )
 
 
 class GracefulHTTPServer(HTTPServer):
@@ -29,7 +33,7 @@ class GracefulHTTPServer(HTTPServer):
 
 
 class CallbackHandler(BaseHTTPRequestHandler):
-    """A request handler for the HTTP server that handles the OAuth callback."""
+    """A request handler for the HTTP server that handles the JWT-auth callback."""
 
     def do_GET(self):
         if self.path.startswith("/callback"):
@@ -41,8 +45,8 @@ class CallbackHandler(BaseHTTPRequestHandler):
             jwt_token = params.get("token")
             refresh_token = params.get("refresh_token")
             if jwt_token and refresh_token:
-                print("Token verified!")
-                store_jwt_securely(jwt_token, refresh_token)
+                print("Token verified.")
+                store_jwt(jwt_token, refresh_token)
                 self.send_response_only(200, "OK")
                 self.send_header("Content-type", "text/html")
                 self.end_headers()
@@ -55,13 +59,12 @@ class CallbackHandler(BaseHTTPRequestHandler):
 
 
 def main():
-    """Main function for the Prefab CLI."""
-    parser = argparse.ArgumentParser(description="Prefab CLI")
+    """Main function for the Prefab authentication CLI."""
+    parser = argparse.ArgumentParser(description="PreFab Auth CLI")
     parser.add_argument("command", help="The command to run", choices=["setup"])
     parser.add_argument(
         "--port", help="Port number for the HTTP server", type=int, default=8000
     )
-
     args = parser.parse_args()
 
     if args.command == "setup":
